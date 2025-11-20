90b9c373b6080b377f666c49a4f05c5049a8493d
Cal <callumburgoyne04@gmail.com>
2025-11-20 18:27:53 +0000

    feat: add discount code eligibility check function

--- backend/internal/checkout/discount.go
+++ backend/internal/checkout/discount.go
@@ -1,5 +1,10 @@
 package checkout
 
+import (
+	"github.com/CallumB04/clothing-shop/backend/internal/data"
+	"github.com/CallumB04/clothing-shop/backend/internal/models"
+)
+
 // Calculate total price of basket after discount
 func CalculateBasketTotalAfterDiscount(price float64, discount float64) float64 {
 	if price <= 0.0 {
@@ -15,4 +20,32 @@ func CalculateBasketTotalAfterDiscount(price float64, discount float64) float64 
 	}
 
 	return price * (1.0 - discount)
+}
+
+// Check if discount code is active, inactive or doesnt exist in discount codes data
+// Returns eligibility and value (if active)
+func CheckDiscountCodeEligibility(code string) (models.DiscountCodeState, float64) {
+	dcData, err := data.LoadDiscountCounts()
+
+	// Return invalid if discount code data load errors
+	if err != nil {
+		return models.DiscountCodeInvalid, 0.0
+	}
+
+	// Check active discount codes
+	for _, dc := range dcData.Active {
+		if dc.Code == code {
+			return models.DiscountCodeActive, dc.Value
+		}
+	}
+
+	// Check inactive discount codes
+	for _, dc := range dcData.Inactive {
+		if dc == code {
+			return models.DiscountCodeInactive, 0.0
+		}
+	}
+
+	// Return invalid if discount code was not found
+	return models.DiscountCodeInvalid, 0.0
 }
