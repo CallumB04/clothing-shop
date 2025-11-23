7df05f6c4b47e6fb3700998c0758ac52e4a63879
Cal <callumburgoyne04@gmail.com>
2025-11-23 06:57:01 +0000

    feat: add component for checkbox

---
+++ frontend/src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,28 @@
+import Icon from "../Icon/Icon";
+import { DarkText } from "../Text/DarkText";
+
+interface CheckboxProps {
+    label?: string;
+    className?: string;
+    checked: boolean;
+    toggleChecked: () => void;
+}
+
+const Checkbox: React.FC<CheckboxProps> = ({
+    label,
+    className,
+    checked,
+    toggleChecked,
+}) => {
+    return (
+        <span
+            className={`flex cursor-pointer items-center gap-1 ${className}`}
+            onClick={toggleChecked}
+        >
+            <Icon icon={checked ? "check_box" : "check_box_outline_blank"} />
+            {label && <DarkText className="text-sm">{label}</DarkText>}
+        </span>
+    );
+};
+
+export default Checkbox;
